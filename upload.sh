d82b8e9d770a698e42747660486291dfb2cc10d8
JahnaviHeadlight <45392277+JahnaviHeadlight@users.noreply.github.com>
2018-12-14 14:19:33 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -4,11 +4,9 @@ set -eu
 set o pipefile
 
 
-curl https://stedolan.github.io/jq/download/linux64/jq 
-
 STATE_FILE=.serverless/serverless-state.json
-S3_PREFIX= $( jq -r '.package.artifactDirectoryName' < "$STATE_FILE")
-ARTIFACT= $( jq -r '.package.artifact' < "$STATE_FILE")
+S3_PREFIX= $( jq ' -r .package.artifactDirectoryName' < "$STATE_FILE")
+ARTIFACT= $( jq '-r .package.artifact' < "$STATE_FILE")
 AWS_PROFILE=${AWS_PROFILE:-}
 
 PROFILE_OPTS=""
